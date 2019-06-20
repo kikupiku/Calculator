3b3d5cba2ff97d00f07fd13966f34586458b0542
Katarzyna Kaswen-Wilk <kml.wilk@gmail.com>
2019-06-20 20:51:14 +0200

    partially refactor mouse event function

--- script.js
+++ script.js
@@ -5,87 +5,82 @@ let friendlyMessage = document.querySelector('h2');
 let display = document.querySelector('.display');
 let keyboard = document.querySelector('.keyboard');
 
-document.addEventListener('keydown', logKey);
+document.querySelectorAll('button').forEach((button) => {
+  button.addEventListener('mousedown', playSound);
+});
+
+function playSound(argument) {
+  let press = new Audio();
+  press.src = "button-press.mp3";
+  press.currentTime = 0;
+  press.play();
+}
+
+function setNewState(message, displayContent, newOutput) {
+  friendlyMessage.textContent = message;
+  display.innerHTML = displayContent;
+  output = newOutput;
+
+  display.innerHTML = (output.length > 12) ? (output.substring(0, 10) + "…"): display.innerHTML;
+}
 
 keyboard.addEventListener('click', (e) => {
-  if (!e.target.classList.contains('button')) {
-    ;                                 //so that nothing happens when I click a non-button area
-  } else {
+  if (e.target.classList.contains('button')) {
     switch (e.target.innerHTML) {
     case 'AC':
-      friendlyMessage.textContent = "OK, let's start over.";
-      display.innerHTML = "0";
-      output = "";
+      setNewState("OK, let's start over.", "0", "");
+
       break;
     case '÷':
-      friendlyMessage.textContent = "";
-      output = output + '/';
-      display.innerHTML = output;
+      setNewState("", (output + '/'), (output + '/'))
       break;
     case '×':
-      friendlyMessage.textContent = "";
-      output = output + '*';
-      display.innerHTML = output;
+      setNewState("", (output + '*'), (output + '*'));
       break;
     case '⌫':
-      friendlyMessage.textContent = "Let's take that back.";
-      let howLong = display.innerHTML;
+      let howLong = output;
       if (howLong.length === 1) {
-        display.innerHTML = "0";
-        output = "";
+        setNewState("A very good place to start...", '0', '');
       } else {
-        output = display.innerHTML;
-        friendlyMessage.textContent = "";
-        let less = output.substring(0, (output.length -1));
-        display.innerHTML = less;
-        output = display.innerHTML;
-        display.innerHTML = (output.length > 12) ? (output.substring(0, 10) + "…"): output;
+        let oldOutput = output;
+        let cutOutput = oldOutput.substring(0, (oldOutput.length - 1));
+        setNewState("Let's take that back.", cutOutput, cutOutput);
       }
       break;
     case '=':
-      if (display.innerHTML.includes("/0")) {
-        friendlyMessage.textContent = "I see you trying to destroy the world. Not today, Dormammu. Not today."
-        display.innerHTML = "0";
-        output = "";
+      if (output.includes("/0")) {
+        setNewState("I see you trying to destroy the world. Not today, Dormammu. Not today.", "0", "");
       } else {
-        output = display.innerHTML;
-        friendlyMessage.textContent = "";
-        display.innerHTML = eval(output);
-        output = display.innerHTML;
-        display.innerHTML = (output.length > 12) ? (output.substring(0, 10) + "…"): output;
+        let oldOutput = output;
+        let newOutput = eval(oldOutput);
+        setNewState("", newOutput, newOutput);
       }
       break;
     case '.':
-      friendlyMessage.textContent = "";
-      if (display.innerHTML.includes('.') && !display.innerHTML.includes("+") && !display.innerHTML.includes("-") && !display.innerHTML.includes("*") && !display.innerHTML.includes("/")) {
-        ;
-        friendlyMessage.textContent = 'No more dots on the left side of the equation, thank you.';
-      } else if (display.innerHTML.includes('.') && (display.innerHTML.includes("+") || display.innerHTML.includes("-") || display.innerHTML.includes("*") || display.innerHTML.includes("/"))) {
-        equationSides = display.innerHTML.replace('-', '+').replace('*', '+').replace('/', '+').split('+');
-        for (let i = 1; i < equationSides.length; i++) {
-          if (equationSides[i].includes('.')) {
-            friendlyMessage.textContent = 'We have enough dots for now. If we need any more dots, I call.';
-            ;
-          } else {
-            display.innerHTML = display.innerHTML + '.';
-            output = display.innerHTML;
-            friendlyMessage.textContent = 'I guess this dot can slip by. But pay heed, you may enter no more dots for this number.';
-          }
+      let oldOutput = output;
+      let subbedSymbolsOutput = '';
+      for (let i = 0; i < oldOutput.length; i++) {
+        if (['-', '*', '/'].includes(oldOutput[i])) {
+          subbedSymbolsOutput += '+';
+        } else {
+          subbedSymbolsOutput += oldOutput[i];
         }
+      }
+      let equationSidesArray = oldOutput.split('+');
+      if (equationSidesArray[equationSidesArray.length - 1].includes('.')) {
+        setNewState('We have enough dots for now. If we need any more dots, I call.', oldOutput, oldOutput);
       } else {
-        display.innerHTML = display.innerHTML + '.';
-        output = display.innerHTML;
+        setNewState('I guess this dot can slip by. But pay heed, you may enter no more dots for this number.', (oldOutput + '.'), (oldOutput + '.'));
       }
       break;
     default:
-      friendlyMessage.textContent = "";
-      output += e.target.innerHTML;
-      display.innerHTML = (output.length > 12) ? (output.substring(0, 10) + "…"): output;
+      setNewState("", (output + e.target.innerHTML), (output + e.target.innerHTML));
       break;
     }
   }
 });
 
+document.addEventListener('keydown', logKey);
 function logKey(e) {
   key = `${e.code}`;
   console.log(key);
@@ -114,12 +109,12 @@ function logKey(e) {
       let numbah = key.split("").pop();
       console.log(numbah);
       output += numbah;
-      display.innerHTML = output;
+      display.innerHTML = (output.length > 12) ? (output.substring(0, 10) + "…"): output;
       break;
     case 'NumpadAdd':
       friendlyMessage.textContent = "";
       output += '+';
-      display.innerHTML = output;
+      display.innerHTML = (output.length > 12) ? (output.substring(0, 10) + "…"): output;
       break;
     case 'NumpadSubtract':
     case 'Minus':
@@ -178,13 +173,13 @@ function logKey(e) {
             friendlyMessage.textContent = 'We have enough dots for now. If we need any more dots, I call.';
             ;
           } else {
-            display.innerHTML = display.innerHTML + '.';
+            display.innerHTML = (output.length > 12) ? (output.substring(0, 10) + "…"): display.innerHTML + '.';
             output = display.innerHTML;
             friendlyMessage.textContent = 'I guess this dot can slip by. But pay heed, you may enter no more dots for this number.';
           }
         }
       } else {
-        display.innerHTML = display.innerHTML + '.';
+        display.innerHTML = (output.length > 12) ? (output.substring(0, 10) + "…"): display.innerHTML + '.';
         output = display.innerHTML;
       }
       break;
@@ -193,10 +188,4 @@ function logKey(e) {
     }
   }
 
-
-function play() {
-  let press = new Audio();
-  press.src = "button-press.mp3";
-  press.currentTime = 0;
-  press.play();
-}
+// TODO fix the too many numbers display bug, and assign new output variable names
